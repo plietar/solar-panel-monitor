cbf62571d01a7a4d1ce08276563375ed46220276
Paul Lietar <paul@lietar.net>
2024-04-21 03:30:33 +0100

    Add Github workflow

--- flake.nix
+++ flake.nix
@@ -3,49 +3,33 @@
     nixpkgs.url = "nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
     esp-dev = {
-      url = "/Users/paul/Projects/CalanMai/nixpkgs-esp-dev/";
+      url = "github:mirrexagon/nixpkgs-esp-dev";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    esp-compilers = {
-      url = "/Users/paul/Projects/nix-esp";
+    esp-toolchain = {
+      url = "github:plietar/nixpkgs-esp-toolchain";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { self, nixpkgs, esp-dev, esp-compilers, flake-utils }:
+  outputs = { self, nixpkgs, esp-dev, esp-toolchain, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
-          overlays = [
-            (self: super: {
-              python310 = super.python310.override {
-                packageOverrides = pself: psuper: {
-                  objgraph = psuper.objgraph.overrideAttrs (old: { doInstallCheck = false; });
-                  urllib3 = psuper.urllib3.overrideAttrs (old: rec {
-                    version = "1.26.16";
-                    format = "setuptools";
-                    buildInputs = [ pself.setuptools ];
-                    src = pself.fetchPypi {
-                      inherit (old) pname;
-                      inherit version;
-                      hash = "sha256-jxNfZQJ1a95rKpsomJ31++h8mXDOyqaQQe3M5/BYmxQ=";
-                    };
-                  });
-                };
-              };
-            })
-          ];
-        };
-      in
-      {
+      let pkgs = import nixpkgs { inherit system; }; in {
+        packages.interactive-html-bom = pkgs.callPackage ./interactive-html-bom.nix { };
+
         devShells.default = pkgs.mkShell {
           buildInputs = [
             (esp-dev.packages."${system}".esp-idf-full.override {
               python3 = pkgs.python310;
               toolsToInclude = [ ];
             })
-            esp-compilers.packages."${system}".riscv32-esp-gcc
+            esp-toolchain.packages."${system}".riscv32-esp-gcc-20230208
+          ];
+        };
+
+        devShells.ci = pkgs.mkShell {
+          buildInputs = [
+            esp-dev.packages."${system}".esp-idf-esp32c3
           ];
         };
       });
